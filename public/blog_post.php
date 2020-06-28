671fe35c153b5ccdd907cd2f8b03a061dc63dd0f
Naowas Morshed Eimon <naowas.morshed.eimon@gmail.com>
2020-06-28 17:24:06 +0600

    Sql collection has bee changed unicode

--- public/blog_post.php
+++ public/blog_post.php
@@ -94,14 +94,14 @@ include 'portfolio.php';
             <a id="config-trigger" class="config-trigger config-panel-hide text-center" href="#"><i class="fas fa-cog fa-spin mx-auto" data-fa-transform="down-6" ></i></a>
             <h5 class="panel-title">Choose Colour</h5>
             <ul id="color-options" class="list-inline mb-0">
-                <li class="theme-1 active list-inline-item"><a data-style="src/frontend/css/theme-1.css" href="#"></a></li>
-                <li class="theme-2  list-inline-item"><a data-style="src/frontendcss/theme-2.css" href="#"></a></li>
-                <li class="theme-3  list-inline-item"><a data-style="src/frontend/css/theme-3.css" href="#"></a></li>
-                <li class="theme-4  list-inline-item"><a data-style="src/frontend/css/theme-4.css" href="#"></a></li>
-                <li class="theme-5  list-inline-item"><a data-style="src/frontend/css/theme-5.css" href="#"></a></li>
-                <li class="theme-6  list-inline-item"><a data-style="src/frontend/css/theme-6.css" href="#"></a></li>
-                <li class="theme-7  list-inline-item"><a data-style="src/frontend/css/theme-7.css" href="#"></a></li>
-                <li class="theme-8  list-inline-item"><a data-style="src/frontend/css/theme-8.css" href="#"></a></li>
+                <li class="theme-1 active list-inline-item"><a data-style="../src/frontend/css/theme-1.css" href="#"></a></li>
+                <li class="theme-2  list-inline-item"><a data-style="../src/frontend/css/theme-2.css" href="#"></a></li>
+                <li class="theme-3  list-inline-item"><a data-style="../src/frontend/css/theme-3.css" href="#"></a></li>
+                <li class="theme-4  list-inline-item"><a data-style="../src/frontend/css/theme-4.css" href="#"></a></li>
+                <li class="theme-5  list-inline-item"><a data-style="../src/frontend/css/theme-5.css" href="#"></a></li>
+                <li class="theme-6  list-inline-item"><a data-style="../src/frontend/css/theme-6.css" href="#"></a></li>
+                <li class="theme-7  list-inline-item"><a data-style="../src/frontend/css/theme-7.css" href="#"></a></li>
+                <li class="theme-8  list-inline-item"><a data-style="../src/frontend/css/theme-8.css" href="#"></a></li>
             </ul>
             <a id="config-close" class="close" href="#"><i class="fa fa-times-circle"></i></a>
         </div><!--//panel-inner-->
@@ -115,18 +115,18 @@ include 'portfolio.php';
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/highlight.min.js"></script>
 
     <!-- Custom JS -->
-    <script src="src/frontend/js/blog.js"></script>
+    <script src="../src/frontend/js/blog.js"></script>
     
     <!-- Style Switcher (REMOVE ON YOUR PRODUCTION SITE) -->
-    <script src="src/frontend/js/demo/style-switcher.js"></script>     
+    <script src="../src/frontend/js/demo/style-switcher.js"></script>     
 
 	    <!-- Javascript -->          
-		<script src="src/frontend/plugins/jquery-3.4.1.min.js"></script>
-    <script src="src/frontend/plugins/popper.min.js"></script> 
-    <script src="src/frontend/plugins/bootstrap/js/bootstrap.min.js"></script> 
+		<script src="../src/frontend/plugins/jquery-3.4.1.min.js"></script>
+    <script src="../src/frontend/plugins/popper.min.js"></script> 
+    <script src="../src/frontend/plugins/bootstrap/js/bootstrap.min.js"></script> 
 
     <!-- Style Switcher (REMOVE ON YOUR PRODUCTION SITE) -->
-    <script src="src/frontend/js/demo/style-switcher.js"></script>  
+    <script src="../src/frontend/js/demo/style-switcher.js"></script>  
     
 
 </body>
--- sqldump/oopblog.sql
+++ sqldump/oopblog.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jun 26, 2020 at 05:50 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.19
+-- Host: localhost
+-- Generation Time: Jun 28, 2020 at 01:23 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -71,7 +72,7 @@ CREATE TABLE `tbl_portfolio` (
 --
 
 INSERT INTO `tbl_portfolio` (`id`, `name`, `description`, `link`, `image_path`) VALUES
-(27, 'Naowas Morshed Eimon', 'PHP Developer', 'naowas.github.io', 'uploads/my.jpg');
+(28, 'Naowas Morshed', 'Php Developer', 'naowas.live', 'uploads/my.jpg');
 
 -- --------------------------------------------------------
 
@@ -81,10 +82,10 @@ INSERT INTO `tbl_portfolio` (`id`, `name`, `description`, `link`, `image_path`) 
 
 CREATE TABLE `tbl_post` (
   `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` text NOT NULL,
+  `title` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -92,7 +93,10 @@ CREATE TABLE `tbl_post` (
 --
 
 INSERT INTO `tbl_post` (`id`, `title`, `body`, `image`, `date`) VALUES
-(1, 'Ask Your Question', '<p><font color=\"#000000\" style=\"background-color: rgb(255, 255, 0);\">rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</font></p>', 'uploads/1593107767f29bd64b-c23c-417c-993f-f929559ae2cc_200x200.png', '2020-06-24 09:00:39');
+(3, ' Lorem Ipsum', '<p style=\"margin-bottom: 15px; text-align: justify; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px;\">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.</p><p style=\"margin-bottom: 15px; text-align: justify; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px;\">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>', 'uploads/posts/15933430251593166176blog-post-thumb-3.jpg', '2020-06-28 11:17:05'),
+(4, ' Where can I get some?', '<p><span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc</span><br></p>', 'uploads/posts/1593343057blog-post-thumb-7.jpg', '2020-06-28 11:17:37'),
+(5, ' What is Lorem Ipsum?', '<p><strong style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">Lorem Ipsum</strong><span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; font-size: 14px; text-align: justify;\">&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</span><br></p>', 'uploads/posts/15933431121593248073off_button.png', '2020-06-28 11:18:32'),
+(6, ' বাংলা ডেমো টেক্সট', '<p style=\"margin-top: 1em; margin-bottom: 1em; color: rgb(0, 0, 0); font-family: kalpurush, sans-serif;\">শব্দ গুলো দেখে সকলেই অবাক হয়েছে, এটা বলা বাহুল্য। অনেকেই ভাবছেন, এ সব শব্দের বেশিরভাগটাই নিজে থেকে বানানো। তবে সত্যটা হলো, এর একটাও বানানো নয়। সব গুলোই বাংলা ভাষার অংশ। উৎপত্তি অনুসারে বাংলাভাষার যে শ্রেনীবিভাগ রয়েছে, সেই পাঁচটি শ্রেনী বিভাগের ফলেই জন্ম নিয়েছে এই সকল শব্দ, যার অনেকগুলোই আপনি আপনার এই পর্যন্ত জীবনে একবারও শোনেননি।<br><br>এখানে বেছে বেছে বানান ও গঠনের দিক থেকে ব্যবহার করতে কঠিন, এমন কিছু শব্দ বাছাই করা হয়েছে। এটি একই সাথে আপনার ব্যবহৃত ফন্ট কতোটা নিঁখুত এবং আপনার ষ্ট্যাইলিং কতোটুকু সঠিক হয়েছে, নির্ধারন করতে সহায়তা করবে।</p><h3 style=\"font-size: 1.17em; margin-top: 1em; margin-bottom: 1em; color: rgb(0, 0, 0); font-family: kalpurush, sans-serif;\">কৃতজ্ঞতা স্বীকার</h3><p style=\"margin-top: 1em; margin-bottom: 1em; color: rgb(0, 0, 0); font-family: kalpurush, sans-serif;\"></p><ul style=\"margin-top: 1em; margin-bottom: 1em; padding-left: 40px; color: rgb(0, 0, 0); font-family: kalpurush, sans-serif;\"><li>অভ্র টিমঃ যাদের জন্য বাংলা আজ সবার কাছে</li><li>সারিম খানঃ অসম্ভব কে সম্ভব করার খারাপ গুণ আছে এই ব্যাক্তিটির</li><li>প্রলয় হাসানঃ কম্পিউটার এর এক কোনায় ফেলে রাখা আইডিয়াটা বাস্তবে নিয়ে আসার পেছনে অনুপ্রেরণার জন্য</li><li>এবং তাদের প্রতি, যারা বাংলা ভাষাকে সকলের কাছে পৌছে দেওয়ার জন্য দিন রাত পরিশ্রম করে যাচ্ছেন। বাংলা লিখছেন, অন্যকে লিখতে উৎসাহিত করছেন।</li></ul>', 'uploads/posts/1593343322f29bd64b-c23c-417c-993f-f929559ae2cc_200x200.png', '2020-06-28 11:22:02');
 
 -- --------------------------------------------------------
 
@@ -171,13 +175,13 @@ ALTER TABLE `tbl_cat`
 -- AUTO_INCREMENT for table `tbl_portfolio`
 --
 ALTER TABLE `tbl_portfolio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `tbl_post`
 --
 ALTER TABLE `tbl_post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
